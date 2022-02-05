a09899dfe1049af243f1378fd4c35cd9ac819de3
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-02-05 11:22:03 -0800

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,46 @@
-
+import React from 'react';
+import Tasks from './Task';
+import { Paper, TextField } from '@material-ui/core';
+import { Checkbox, Button } from '@material-ui/core';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>To do app</h1>
-    </div>
-  );
+class App extends Tasks {
+  state = { tasks: [], currentTask: ""  } 
+  render() { 
+    const {tasks} = this.state;
+    return (
+      <div className='App flex'>
+        <Paper elevation={3} className="container">
+          <div className='heading'>
+          TO DO
+          </div>
+          <form onSubmit={this.handleSubmit} className='flex'
+          style={{margin: "15px 0"}}>
+            <TextField variant="outlined" size='medium' style={{width: "50%"}} value={this.state.currentTask} required={true} onChange={this.handleChange}
+            placeholder="ADD NEW TODO"/>
+            <Button style={{height: "40px"}} color="primary" variant='outlined' type='submit'>
+              ADD
+            </Button>
+          </form>
+          <div>
+            {tasks.map(task => (
+              <Paper key={task._id} className="flex task_container">
+              <Checkbox checked={task.completed}
+              onClick={() => this.handleUpdate(task._id)} 
+              color='primary'/>
+              <div className={task.completed ? "task line_through" : "task"}>
+              {task.task}
+              </div>
+              <Button onClick={() => this.handleDelete(task._id)} color="secondary">
+                delete
+              </Button>
+              </Paper>
+            ))}
+          </div>
+        </Paper>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
